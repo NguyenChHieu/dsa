3c1600c0f79667a60de985289695818270d32090
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-10 21:28:43 +0700

    add

--- Tree/1026. Maximum Difference Between Node and Ancestor.py
+++ Tree/1026. Maximum Difference Between Node and Ancestor.py
@@ -9,14 +9,21 @@ class Solution:
         if not root:
             return 0
         
-        # keep track of cur_max, cur_min
-        def dfs(node, curr_max, curr_min):
+        # keep track of cur_max and curmin since when the node is null (external node)
+        # then all the max/min values have been checked and updated correspondingly
+        # along the way. So the max diff will be calculated correctly
+        
+        # the values wouldnt be mixed up with different subtrees since the curmax/min
+        # will only be updated when traverse down the tree - if different subtree then its
+        # in a different recursive branch.
+        def dfs(node, cur_max, cur_min):
             if not node:
-                return curr_max - curr_min
-            curr_max = max(curr_max, node.val)
-            curr_min = min(curr_min, node.val)
-            left = dfs(node.left, curr_max, curr_min)
-            right = dfs(node.right, curr_max, curr_min)
-            return max(left,right)
-
+                return cur_max-cur_min
+            cur_max = max(cur_max, node.val)
+            cur_min = min(cur_min, node.val)
+            
+            left = dfs(node.left, cur_max, cur_min)
+            right = dfs(node.right, cur_max, cur_min)
+            
+            return max(left, right)
         return dfs(root, root.val, root.val)

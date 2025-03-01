17a8c6d8a98d9150da3dba5f6ca800890ad6ee93
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-03-01 15:03:36 +1100

    add

---
+++ Heuristic/2460. Apply Operations to an Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+
+        ls = []
+        count = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                ls.append(nums[i])
+            else:
+                count += 1
+
+        for i in range(count):
+            ls.append(0)
+
+        return ls
